cff9fb48f1c2ebc16c365e585f174e48a3ad42f7
Joy <ziaurjoy802@gmail.com>
2020-10-06 01:10:58 +0600

    login done

--- core/views.py
+++ core/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    users = User.objects.count()
+    return render(request,'home.html',context={'users': users})
+
+def signup(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('home')
+    return render(request, 'registration/signup.html',context={'form':form})
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+    path('signup', views.signup, name = 'signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
